edf49f996090a317f5f3feb7fcb4debe6e61711f
danielcheruiyot <danielcheruiyot88@gmail.com>
2023-03-17 12:35:27 +0300

    Added more keyframe animations

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Wordpress Developer", "SEO Consultant"],
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+
+})
